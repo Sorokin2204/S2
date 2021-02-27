ace8c3515742b9dca464cbc270c66567fdf63b0c
Sorokin2204 <daniil.sorokin.228@gmail.com>
2021-02-27 10:15:44 +0300

    Add adaptive for all section

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,15 @@ $(function () {
     slidesToShow: 1,
     prevArrow: '.reason__nav-left',
     nextArrow: '.reason__nav-right',
+
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          variableWidth: false,
+        },
+      },
+    ],
   });
 
   $('.comments__slider-box').slick({
@@ -32,6 +41,7 @@ $(function () {
     .clone()
     .appendTo('.header__top .container')
     .addClass('header__top-inner--mobile')
+    .css('display', 'none')
     .find('.menu')
     .addClass('menu--mobile')
     .find('.sub-menu')
@@ -43,14 +53,38 @@ $(function () {
     if ($(this).find('.sub-menu').length !== 0) {
       $(this).addClass('menu__item--sub-menu');
       $(this).click(function (e) {
-        e.stopPropagation();
-        $('.menu__item--active')
-          .not($(this))
-          .find('.sub-menu')
-          .toggleClass('sub-menu--active');
-        $('.menu__item--active').not($(this)).toggleClass('menu__item--active');
-        $(this).toggleClass('menu__item--active');
-        $(this).find('.sub-menu').toggleClass('sub-menu--active');
+        if ($(this).parent().hasClass('menu--mobile')) {
+          //    $(this).attr('aria-expanded', false);
+          $(this).attr('disabled', true);
+          var dropDown = $(this).find('sub-menu--mobile');
+
+          $(this).parent().find('.sub-menu--mobile').not(dropDown).slideUp();
+
+          if ($(this).hasClass('.menu-item--active')) {
+            $(this).removeClass('menu-item--active');
+          } else {
+            $(this)
+              .parent()
+              .find('menu-item--active')
+              .removeClass('menu-item--active');
+            $(this).addClass('menu-item--active');
+          }
+
+          dropDown.stop(false, true).slideToggle();
+
+          e.preventDefault();
+        } else {
+          e.stopPropagation();
+          $('.menu__item--active')
+            .not($(this))
+            .find('.sub-menu')
+            .toggleClass('sub-menu--active');
+          $('.menu__item--active')
+            .not($(this))
+            .toggleClass('menu__item--active');
+          $(this).toggleClass('menu__item--active');
+          $(this).find('.sub-menu').toggleClass('sub-menu--active');
+        }
       });
     }
   });
@@ -60,25 +94,13 @@ $(function () {
     $('.menu__item--active').toggleClass('menu__item--active');
   });
 
+  $('.accordion a').click(function (j) {});
+
   $('.menu-btn').on('click', function () {
     $('.menu-over').toggleClass('menu-over--active');
     $('.header__top-inner--mobile').toggleClass(
       'header__top-inner--mobile-active',
     );
-    $('.menu__item').each(function () {
-      if ($(this).find('.sub-menu').length !== 0) {
-        $(this).addClass('menu__item--sub-menu');
-        $(this).click(function (e) {
-          e.stopPropagation();
-          //$('.menu__item--active').not($(this)).find('.sub-menu').slideToggle();
-          $('.menu__item--active')
-            .not($(this))
-            .toggleClass('menu__item--active');
-          $(this).toggleClass('menu__item--active');
-          $(this).find('.sub-menu').slideToggle();
-        });
-      }
-    });
   });
 
   $('.menu-over').on('click', function () {
@@ -89,12 +111,31 @@ $(function () {
   });
 });
 
-$(window).resize(function mobileContainer() {
-  if (window.innerWidth < 640) {
-    $('.header__bottom .container').addClass('container--mobile');
+function tariffsContainer() {
+  if (window.innerWidth <= 640) {
+    $('.tariffs .container').addClass('container--full');
   } else {
-    if ($('.header__bottom .container').hasClass('container--mobile')) {
-      $('.header__bottom .container').removeClass('container--mobile');
+    if ($('.tariffs .container').hasClass('container--full')) {
+      $('.tariffs .container').removeClass('container--full');
     }
   }
+}
+
+function commentsContainer() {
+  if (window.innerWidth <= 960) {
+    $('.comments .container').addClass('container--full');
+  } else {
+    if ($('.comments .container').hasClass('container--full')) {
+      $('.comments .container').removeClass('container--full');
+    }
+  }
+}
+
+$(window).on('load', () => {
+  tariffsContainer();
+  commentsContainer();
+});
+$(window).resize(() => {
+  tariffsContainer();
+  commentsContainer();
 });
